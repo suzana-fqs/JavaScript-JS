9358eef3ef06ecbe63e914a513ff8c74f5b78097
suzana-fqs <147282124+suzana-fqs@users.noreply.github.com>
2024-03-21 16:31:09 -0300

    ex010 Aula 11

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,4 +1,6 @@
-var vel = 88
+var vel = 188
 console.log('Respeite a sinalização.')
     if (vel > 60) //CONDIÇÃO SIMPLES
-        console.log(`Sua velocidade é de ${vel} km/h. Você foi MULTADO!`)
+        console.log(`Sua velocidade é de ${vel}km/h. Você foi MULTADO!`)
+    else (vel > 120)
+        console.log(`VC está querendo se matar? ${vel}km/h... Sério isso?`)
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log (`Nascido em ${pais}?`)
+if (pais == 'Brasil') {
+    console.log (' Vc é Brasileiro!')
+} else {
+    console.log ('Você é estrangeiro!')
+}
