fcba6e1aeeebe831b3ac24c750ae79a8f581c93e
suzana-fqs <147282124+suzana-fqs@users.noreply.github.com>
2024-04-05 15:51:48 -0300

    Exercicio js parte 1 e  2

---
+++ ex-parte2/script.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours
+    msg.innerHTML = (`Agora são ${hora} horas`)
+    if (hora >= 0 && hora < 12){
+        img.src = 'manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >=12 && hora <= 18) {
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#d5876f'
+    } else {
+        img.src = 'noite.n.jpg'
+        document.body.style.background ='#515154'
+    }
+}
