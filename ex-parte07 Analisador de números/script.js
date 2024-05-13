bac812d00c15751d427ee08c5e0c4e45f865d2d0
suzana-fqs <147282124+suzana-fqs@users.noreply.github.com>
2024-05-13 17:04:33 -0300

    Finalização no Analisador de Números - Ultima aula JS

--- ex-parte07 Analisador de números/script.js
+++ ex-parte07 Analisador de números/script.js
@@ -12,7 +12,7 @@ function isNumero(n){
 }
 
 function inLista(n, l){
-    if(l.indexOf(Number(n)) != -1){
+    if(l.indexOf(Number(n)) !== -1){
         return true
     } else{
         return false
@@ -24,10 +24,37 @@ function add(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         lista.appendChild (item)
+        valores.push(Number(num.value)) // adiciona o número a lista de valores  - esse era o comando que estava faltando :O
     } else{
         window.alert ('Valor inválido ou já adicionado na lista.')
     }
     num.value = ''
     num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert ('Adicione valores a lista antes de finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores){
+            soma+= valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML=''
+        res.innerHTML+= `<p> No total temos ${tot} números adicionados.</p>`
+        res.innerHTML+= `<p> O maior valor adicionado foi ${maior}.</p>`
+        res.innerHTML+= `<p>O menor valor adicionado foi ${menor}</p>`
+        res.innerHTML+= `<p>A soma de todos os números é de ${soma}.</p>`
+        res.innerHTML+= `<p>A média é de ${media}.</p>`
+    }
 }
 
